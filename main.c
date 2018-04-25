aaf0462b34372295129a919f1682b185c685aac3
farazmurtaza <k164054@nu.edu.pk>
2018-04-25 22:49:55 +0500

    Second Commit - Added main.c

---
+++ main.c
@@ -0,0 +1,48 @@
+int main()
+{
+
+    for (int i = 0; i < 3; ++i)
+    {
+        semWait[i]=7;
+    }
+	//smoker 1 has tobbaco
+	//smoker 2 has matches
+	//smoker 3 has paper
+    sem_init(&semaphoreSmokers[0],0,0);
+    sem_init(&semaphoreSmokers[1],0,0);
+    sem_init(&semaphoreSmokers[2],0,0);
+
+    sem_init(&semaphoreAgents,0,1);
+
+    sem_init(&agentReady,0,0);
+    sem_init(&smokerReady,0,0);
+   
+
+
+    pthread_t smoker1,smoker2,smoker3,agent1,pusher,agent2,agent3;
+    int * at = new int[1];
+    at[0]=1;
+
+    int * at1 = new int[1];
+    at1[0]=2;
+
+    int * at2 = new int[1];
+    at2[0]=3;
+
+    pthread_create(&smoker1, NULL, smoker,at );
+	
+    pthread_create(&smoker2,NULL,smoker,at1);
+    	
+    pthread_create(&smoker3,NULL,smoker,at2);
+
+    pthread_create(&agent1,NULL,agent,at);
+
+    pthread_create(&pusher,NULL,func,NULL);
+
+    pthread_join(smoker1,NULL);
+    pthread_join(smoker2,NULL);
+    pthread_join(smoker3,NULL);
+
+
+    return 0;
+}
